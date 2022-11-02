3d2ecc47266995854dd34384d512f96b2d622bb8
tuanhn110789 <105765675+tuanhn110789@users.noreply.github.com>
2022-11-02 21:09:32 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask
 
-crt = r'C:\Users\tuanhd\Downloads\Flask-Application-On-IIS-main\tdp-vietnam_com.pem'
-key = r'C:\Users\tuanhd\Downloads\Flask-Application-On-IIS-main\key.pem'
 # create a server instance
 app = Flask(__name__)
 
